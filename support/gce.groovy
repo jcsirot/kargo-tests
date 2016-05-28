008687ecac4f61715d71460eec04b0518cb68b0a
Jean-Christophe Sirot <jcsirot@gmail.com>
2016-05-28 17:35:21 +0200

    uncomment delete stage and set zone to us-central1-a

--- support/gce.groovy
+++ support/gce.groovy
@@ -6,12 +6,11 @@ def run(username, credentials_id, project_id, service_account_email, gce_pem_id,
                 try {
                     create_vm(run_id, project_id, service_account_email, gce_pem_id, image)
                     install_cluster(username, credentials_id, network_plugin)
-
                     //test_apiserver(inventory_path, credentialsId)
                     //test_create_pod(inventory_path, credentialsId)
                     //test_network(inventory_path, credentialsId)
                 } finally {
-                    //delete_vm(run_id, inventory_path, image, credentialsId)
+                    delete_vm(run_id, inventory_path, image, credentialsId)
                 }
             }
         }
@@ -21,7 +20,7 @@ def run(username, credentials_id, project_id, service_account_email, gce_pem_id,
 def create_vm(run_id, project_id, service_account_email, gce_pem_id, image) {
     stage 'Provision'
     withCredentials([[$class: 'FileBinding', credentialsId: gce_pem_id, variable: 'GCE_PEM']]) {
-        sh "kargo gce -y --pem_file ${env.GCE_PEM} --email \"${service_account_email}\" --zone us-central1-c --type \"n1-standard-1\" --image \"${image}\" --project ${project_id} --instances 3"
+        sh "kargo gce -y --pem_file ${env.GCE_PEM} --email \"${service_account_email}\" --zone us-central1-a --type \"n1-standard-1\" --image \"${image}\" --project ${project_id} --instances 3"
     }
 }
 
